0eaf9fac27480ec843e30160ab61c716e51f8022
Carlos Rafael de Oliveira do Amaral Leitao <Carlos Rafael de Oliveira do Amaral Leitao>
2017-01-13 12:37:16 -0200

    SPA com sidebar! Estava dando muitos problemas ao renderizar o ChatContainer por causa do positioning do CSS, mas agora esta funcionando

--- main_site/static/reactjs/layouts/layout.jsx
+++ main_site/static/reactjs/layouts/layout.jsx
@@ -6,8 +6,12 @@ export default class Layout extends React.Component{
 	render(){
 		return(
 
-			<div>
-				<ul className="nav nav-tabs">
+		<div id="wrapper">
+
+        
+        <div id="sidebar-wrapper">
+           
+            <ul className="sidebar-nav">
 				  <li role="presentation" activeClassName="active">
 				  	<Link to="/home">Home</Link>
 				  </li>
@@ -21,9 +25,36 @@ export default class Layout extends React.Component{
 				  	<Link to="/chat">Chat</Link>
 				  </li>
 				</ul>
+        </div>
+        
+        <div id="page-content-wrapper">
+            <div className="container-fluid">
 
-				{this.props.children}
-			</div>
+                {/*<div className="row">
+                                    <div className="col-lg-12">
+                                        <h1>Simple Sidebar</h1>
+                                        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
+                                        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                                        <button className="btn btn-default" id="menu-toggle">Toggle Menu</button>
+                                    </div>
+                                </div>*/}
+                {this.props.children}
+            </div>
+        </div>
+        {/*<!-- /#page-content-wrapper -->*/}
+
+            <script>
+		    {
+		    	`$("#menu-toggle").click(function(e) {
+		    				        e.preventDefault();
+		    				        $("#wrapper").toggleClass("toggled");
+		    				    });`
+			}
+		    </script>
+    </div>
+    
+
+			
 		)
 	}
 }
